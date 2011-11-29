fa693ed6e4e3526d5f4e1ec54c2273d7abbcf74c
Imre Tuske <imre.tuske@gmail.com>
2011-11-29 17:55:04 +0100

    code reformat

--- hAbcGeomExport.h
+++ hAbcGeomExport.h
@@ -41,56 +41,64 @@
 class OP_TemplatePair;
 class OP_VariablePair;
 
-namespace HDK_Sample {
+namespace HDK_Sample
+{
 
-class hAbcGeomExport : public ROP_Node {
-public:
+	class hAbcGeomExport : public ROP_Node
+	{
+	public:
 
-    /// Provides access to our parm templates.
-    static OP_TemplatePair	*getTemplatePair();
-    /// Provides access to our variables.
-    static OP_VariablePair	*getVariablePair();
-    /// Creates an instance of this node.
-    static OP_Node		*myConstructor(OP_Network *net, const char*name,
-						OP_Operator *op);
+		/// Provides access to our parm templates.
+		static OP_TemplatePair *getTemplatePair ();
 
-protected:
-	     hAbcGeomExport(OP_Network *net, const char *name, OP_Operator *entry);
-    virtual ~hAbcGeomExport();
+		/// Provides access to our variables.
+		static OP_VariablePair *getVariablePair ();
 
-    /// Called at the beginning of rendering to perform any intialization 
-    /// necessary.
-    /// @param	nframes	    Number of frames being rendered.
-    /// @param	s	    Start time, in seconds.
-    /// @param	e	    End time, in seconds.
-    /// @return		    True of success, false on failure (aborts the render).
-    virtual int			 startRender(int nframes, float s, float e);
+		/// Creates an instance of this node.
+		static OP_Node *myConstructor (OP_Network * net,
+					       const char *name,
+					       OP_Operator * op);
 
-    /// Called once for every frame that is rendered.
-    /// @param	time	    The time to render at.
-    /// @param	boss	    Interrupt handler.
-    /// @return		    Return a status code indicating whether to abort the
-    ///			    render, continue, or retry the current frame.
-    virtual ROP_RENDER_CODE	 renderFrame(float time, UT_Interrupt *boss);
+	protected:
+		  hAbcGeomExport (OP_Network * net, const char *name,
+				  OP_Operator * entry);
+		  virtual ~ hAbcGeomExport ();
 
-    /// Called after the rendering is done to perform any post-rendering steps
-    /// required.
-    /// @return		    Return a status code indicating whether to abort the
-    ///			    render, continue, or retry.
-    virtual ROP_RENDER_CODE	 endRender();
+		/// Called at the beginning of rendering to perform any intialization 
+		/// necessary.
+		/// @param  nframes     Number of frames being rendered.
+		/// @param  s           Start time, in seconds.
+		/// @param  e           End time, in seconds.
+		/// @return             True of success, false on failure (aborts the render).
+		virtual int startRender (int nframes, float s, float e);
 
-public:
+		/// Called once for every frame that is rendered.
+		/// @param  time        The time to render at.
+		/// @param  boss        Interrupt handler.
+		/// @return             Return a status code indicating whether to abort the
+		///                     render, continue, or retry the current frame.
+		virtual ROP_RENDER_CODE renderFrame (float time,
+						     UT_Interrupt * boss);
 
-    /// A convenience method to evaluate our custom file parameter.
-    void  OUTPUT(UT_String &str, float t)
-    { STR_PARM("file",  0, 0, t) }
+		/// Called after the rendering is done to perform any post-rendering steps
+		/// required.
+		/// @return             Return a status code indicating whether to abort the
+		///                     render, continue, or retry.
+		virtual ROP_RENDER_CODE endRender ();
 
-private:
-    static int		*ifdIndirect;
-    float		 myEndTime;
-};
+	public:
 
-}	// End HDK_Sample namespace
+		/// A convenience method to evaluate our custom file parameter.
+		void OUTPUT (UT_String & str, float t)
+		{
+		STR_PARM ("file", 0, 0, t)}
+
+	private:
+		static int *ifdIndirect;
+		float myEndTime;
+	};
+
+}				// End HDK_Sample namespace
 
 
 #undef STR_PARM
