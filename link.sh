0d0b5f70d1b56472cfb5a05b2026a872c1370af1
Imre Tuske <imre.tuske@gmail.com>
2011-12-12 20:47:51 +0100

    placed main func in separate file (allows reloading of the code without houdini restart)

--- link.sh
+++ link.sh
@@ -3,9 +3,11 @@ S=`pwd`/soho
 T=$HIH/soho
 
 rm -rf $T/hAbcExport.py
+rm -rf $T/hAbcExportFrame.py
 rm -rf $T/*.pyc
 
 ln -sf $S/hAbcExport.py $T
+ln -sf $S/hAbcExportFrame.py $T
 
 #mkdir -p $T/parameters
 
--- soho/hAbcExport.py
+++ soho/hAbcExport.py
@@ -4,15 +4,8 @@
 #
 #
 
-import time
-import soho
+
 import sys
-import string
-
-from soho import Precision
-from soho import SohoParm
-
-import hou
 
 
 def msg(m):
@@ -24,92 +17,12 @@ def dbg(m):
 
 
 
-
-dbg("hAbcExport.py -- RUNNING")
-
-
-
-def collect_archy( objname, archy=None, level=1 ):
-
-	n = hou.node(objname)
-
-	if n:
-		if archy is None: archy = []
-		dbg( " %s %s"  %  ('-' * level, n.path() ) )
-		archy.append(n.path())
-
-		cs = [ c.path() for c in n.outputs() ]
-		for c in cs:
-			archy = collect_archy(c, archy, level+1)
-		return archy
-	
-	return []
+import hAbcExportFrame
+reload(hAbcExportFrame)
 
 
 
-
-
-def export():
-	dbg("1")
-
-	ps = soho.evaluate({
-		'now':		SohoParm('state:time',			'real', [0],  False, key='now'),
-		'fps':		SohoParm('state:fps',			'real', [24],  False, key='fps'),
-		'hver':		SohoParm('state:houdiniversion',	'string', [''],  False, key='hver'),
-		'objpath':	SohoParm('objpath',		'string',	[''], False),
-		'abcoutput':	SohoParm('abcoutput',		'string',	[''], False),
-		'trange':	SohoParm('trange',		'int',		[0], False),
-		'f':		SohoParm('f',			'int',		None, False)
-	})
-	
-	dbg("2")
-
-	now = ps['now'].Value[0]
-	fps = ps['fps'].Value[0]
-	hver = ps['hver'].Value[0]
-
-	if not soho.initialize(now):
-		soho.error("couldn't initialize")
-		return
-
-	frame = int(now*fps)+1
-
-	objpath = ps['objpath'].Value[0]
-	abcoutput = ps['abcoutput'].Value[0]
-	trange = ps['trange'].Value[0]
-	f = ps['f'].Value
-
-	dbg("3")
-
-	dbg("now=%.3f fps=%.3f -> %.3f" % (now, fps, frame))
-
-	dbg("objpath=%s abcoutput=%s trange=%d f=%s" % \
-		(objpath, abcoutput, trange, str(f)))
-
-	soho.addObjects(now, '*', '*', '', True)
-	soho.lockObjects(now)
-
-	dbg("4")
-
-	objlist = soho.objectList('objlist:instance')
-
-	dbg("5")
-
-	sop_dict = {} # {objname: sopname}
-
-	for obj in objlist:
-		dbg(" -- %s" % obj.getName())
-		path = obj.getDefaultedString('object:soppath', now, [''])[0]
-		dbg(" ---- %s" % path)
-		sop_dict[obj.getName()] = path
-
-	archy = collect_archy(objpath)
-
-	for obj in archy:
-		pass
-
-
-export()
+hAbcExportFrame.export()
 
 
 
