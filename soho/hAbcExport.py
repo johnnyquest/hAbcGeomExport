f970e9c3ed7da6f83d108728534bfe94387906bc
Imre Tuske <imre.tuske@gmail.com>
2011-12-12 16:01:17 +0100

    working on SOHO part of exporter

--- soho/hAbcExport.py
+++ soho/hAbcExport.py
@@ -12,7 +12,50 @@ import string
 from soho import Precision
 from soho import SohoParm
 
+import hou
 
-sys.__stderr__.write("hullo-------------\n")
+def msg(m):
+	sys.__stderr__.write("%s\n" % str(m))
+
+
+
+
+
+msg("hAbcExport.py -- RUNNING")
+
+
+if True:
+	msg("1")
+	
+	ps = soho.evaluate({
+		'now':		SohoParm('state:time',			'real', [0],  False, key='now'),
+		'fps':		SohoParm('state:fps',			'real', [24],  False, key='fps'),
+		'hver':		SohoParm('state:houdiniversion',	'string', [''],  False, key='hver'),
+		'objpath':	SohoParm('objpath',		'string',	[''], False),
+		'abcoutput':	SohoParm('abcoutput',		'string',	[''], False),
+		'trange':	SohoParm('trange',		'int',		[0], False),
+		'f':		SohoParm('f',			'int',		None, False)
+	})
+	
+	msg("2")
+
+	now = ps['now'].Value[0]
+	fps = ps['fps'].Value[0]
+	hver = ps['hver'].Value[0]
+
+	frame = int(now*fps)+1
+
+	objpath = ps['objpath'].Value[0]
+	abcoutput = ps['abcoutput'].Value[0]
+	trange = ps['trange'].Value[0]
+	f = ps['f'].Value
+
+	msg("3")
+
+	msg("now=%.3f fps=%.3f -> %.3f" % (now, fps, frame))
+
+	msg("objpath=%s abcoutput=%s trange=%d f=%s" % \
+		(objpath, abcoutput, trange, str(f)))
+
 
 
