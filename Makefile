4d9414ac783218680879afa9bcce80cd1bbd1c2d
Imre Tuske <imre.tuske@gmail.com>
2011-12-14 14:31:41 +0100

    Makefile-based build

---
+++ Makefile
@@ -0,0 +1,42 @@
+# Makefile.
+#
+
+DSONAME = $(HIH)/dso/hAbcGeomExport.so
+
+
+SOURCES = src/hAbcGeomExport.cpp
+
+#INCDIRS = \
+#	-I/usr/include/OpenEXR \
+#	-I/home/tusi/work/dev/alembic/libs/linux/include \
+#	-I/home/tusi/work/dev/alembic/libs/linux/alembic-0.9.3/include
+#
+#LIBDIRS = \
+#	-L/home/tusi/work/dev/alembic/libs/linux/lib \
+#	-L/home/tusi/work/dev/alembic/libs/linux/alembic-0.9.3/lib/static
+
+INCDIRS = \
+	-I/usr/include/OpenEXR \
+	-I/home/tusi/work/alembic/libs/linux/include \
+	-I/home/tusi/work/alembic/libs/linux/alembic-1.0.3/include
+
+LIBDIRS = \
+	-L/home/tusi/work/alembic/libs/linux/lib \
+	-L/home/tusi/work/alembic/libs/linux/alembic-1.0.3/lib/static
+
+LIBS = \
+	-lHalf -lIex -lhdf5 -lhdf5_hl \
+	-lAlembicAbcCoreHDF5 -lAlembicAbcCoreAbstract \
+	-lAlembicAbc -lAlembicAbcGeom -lAbcWFObjConvert -lAlembicUtil
+
+
+
+###############################################################################
+# For GNU make, use this line:
+#      include $(HFS)/toolkit/makefiles/Makefile.gnu
+# For Microsoft Visual Studio's nmake use this line instead
+#      !INCLUDE $(HFS)/toolkit/makefiles/Makefile.nmake
+#
+include $(HFS)/toolkit/makefiles/Makefile.gnu
+
+
--- build.sh
+++ build.sh
@@ -8,11 +8,6 @@
 #	* This script should be run from a shell with a Houdini
 #	environment initialized.
 #
-#	* If any of the linked libraries use additional .so libs
-#	(not entirely statically linked), those .so file should
-#	be in the library path, or the LD_LIBRARY_PATH variable
-#	should be set up explicitly to their location.
-#
 #	TODO:
 #
 #	A waf version.
@@ -20,43 +15,14 @@
 #
 
 
-# set these variables accordingly
-#
-HOUDINI_PREFS=$HIH
-#OPENEXR_INCLUDES=~/work/dev/alembic/libs/linux/include/OpenEXR
-#ALEMBIC_SUPPORT_LIBS=~/work/dev/alembic/libs/linux/lib
-#ALEMBIC_SUPPORT_INCLUDES=~/work/dev/alembic/libs/linux/include
-#ALEMBIC_LIBS=~/work/dev/alembic/libs/linux/alembic-0.9.3/lib/static
-#ALEMBIC_INCLUDES=~/work/dev/alembic/libs/linux/alembic-0.9.3/include
-
-OPENEXR_INCLUDES=/usr/include/OpenEXR
-
-ALEMBIC_SUPPORT_LIBS=~/work/alembic/libs/linux/lib
-#ALEMBIC_SUPPORT_LIBS=$HFS/dsolib
-
-ALEMBIC_SUPPORT_INCLUDES=~/work/alembic/libs/linux/include
-ALEMBIC_LIBS=~/work/alembic/libs/linux/alembic-1.0.3/lib/static
-ALEMBIC_INCLUDES=~/work/alembic/libs/linux/alembic-1.0.3/include
-
-
 # remove previous build
 #
 rm -f $HOUDINI_PREFS/dso/hAbcGeomExport.so
 
 
-# perform build (link library order is important!)
-#
-export SESI_TAGINFO="Johnny Quest / Questetics Inc."
-hcustom -g \
-	-L $ALEMBIC_SUPPORT_LIBS \
-	-l Half -l Iex -l hdf5 -l hdf5_hl \
-	-L $ALEMBIC_LIBS \
-	-l AlembicAbcCoreHDF5 -l AlembicAbcCoreAbstract \
-	-l AlembicAbc -l AlembicAbcGeom -l AbcWFObjConvert -l AlembicUtil \
-	-I $OPENEXR_INCLUDES \
-	-I $ALEMBIC_INCLUDES \
-	-I $ALEMBIC_SUPPORT_INCLUDES \
-	src/hAbcGeomExport.cpp
+make clean
+make
+
 
 # delete leftover stuff
 #
